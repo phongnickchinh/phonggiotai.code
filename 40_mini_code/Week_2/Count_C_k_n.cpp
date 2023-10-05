78c115bc4d6cbcb8e0148558d436e2c71f903638
phonggiotai <114666846+phonggiotai@users.noreply.github.com>
2023-10-06 01:34:16 +0700

    jhjh

--- 40_mini_code/Week_2/Count_C_k_n.cpp
+++ 40_mini_code/Week_2/Count_C_k_n.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define N 1000
 const int MOD = 1e9 + 7;
 int Remember_ar[N][N];
+//ở đây dùng đệ quy có nhớ để tính C(k,n) bằng cách tìm các giá trị từ C(0,0) lưu vào Remember_ar
 
 void C_k_n( int k, int n){
 for (int i = 0; i <=n; i++){
